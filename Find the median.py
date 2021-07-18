38f3f40b2cc5d64ea2e1cb64c2723ff8d600ff5e
Harshit Poddar <harshitpoddar09@gmail.com>
2021-07-18 22:05:30 +0530

    Add files via upload

---
+++ Find the median.py
@@ -0,0 +1,22 @@
+class Solution:
+    def find_median(self, v):
+        # Code here
+        v=sorted(v)
+        if len(v)%2==0:
+            return (v[len(v)//2]+v[(len(v)//2)-1])//2
+        else:
+            return v[len(v)//2]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        n = int(input())
+        v = list(map(int,input().split())) 
+        ob = Solution();
+        ans = ob.find_median(v)
+        print(ans)
+# } Driver Code Ends
