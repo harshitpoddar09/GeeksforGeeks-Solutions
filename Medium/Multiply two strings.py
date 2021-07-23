101e6c819b81e38c9aad192b6227c8fca937aaf5
Harshit Poddar <harshitpoddar09@gmail.com>
2021-07-23 21:43:09 +0530

    Add files via upload

---
+++ Medium/Multiply two strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def multiplyStrings(self,s1,s2):
+        # code here
+        # return the product string
+        return int(s1)*int(s2)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        a,b=input().split()
+        print(Solution().multiplyStrings( a.strip() , b.strip() ))
+
+# } Driver Code Ends
