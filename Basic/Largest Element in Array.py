98d542b5bc1f5c654394af150d15cb1f81f7e69e
Harshit Poddar <harshitpoddar09@gmail.com>
2021-11-11 21:45:08 +0530

    Add files via upload

---
+++ Basic/Largest Element in Array.py
@@ -0,0 +1,30 @@
+def largest( arr, n):
+    return max(arr)
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(largest(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
