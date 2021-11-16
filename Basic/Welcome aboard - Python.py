42a93545ba0fcb12627e2a193cab28bfd1494857
Harshit Poddar <harshitpoddar09@gmail.com>
2021-11-16 10:54:02 +0530

    Add files via upload

---
+++ Basic/Welcome aboard - Python.py
@@ -0,0 +1,30 @@
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+# Function to Welcome the person
+def welcomeAboard(name):
+    print ('Welcome',name) # Your code here
+   
+
+#{ 
+#Driver Code Starts.
+# Driver Code
+def main():
+    
+    # Testcase input
+    testcases = int(input())
+    
+    # Looping through testcases
+    while(testcases > 0):
+        name = input()
+        
+        welcomeAboard(name);
+        
+        testcases -= 1
+ 
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends
