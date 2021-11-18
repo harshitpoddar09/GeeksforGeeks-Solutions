b61a3eda2803c90b8d21675d60e3b5e3c2b9c0d1
Harshit Poddar <harshitpoddar09@gmail.com>
2021-11-18 13:17:59 +0530

    Add files via upload

---
+++ Basic/Taking input - Python.py
@@ -0,0 +1,30 @@
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+def inPutCat():
+    a = str(input())##complete this
+    b = str(input())##complete this
+    c = str(input())##complete this
+    print(a, b, c) ## comma is used as it automatically separates the variables by a space. 
+    ## + can also be used to concatenate but it would require manual spaces to print the words with spaces between them.
+
+
+#{ 
+#Driver Code Starts.
+
+
+
+def main():
+    testcases=int(input()) #testcases
+    while(testcases>0):
+        inPutCat() #the function that gets things done
+        testcases-=1
+        
+
+
+if __name__=='__main__':
+    main()
+#} Driver Code Ends
