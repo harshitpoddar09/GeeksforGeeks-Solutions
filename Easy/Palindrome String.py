fde81c003bfe4db4e92e2edbb34521ea9d3cebde
Harshit Poddar <harshitpoddar09@gmail.com>
2021-11-22 11:23:31 +0530

    Add files via upload

---
+++ Easy/Palindrome String.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPlaindrome(self, S):
+        # code here
+        if S==S[::-1]:
+            return 1
+        return 0
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        S = input()
+        ob = Solution()
+        answer = ob.isPlaindrome(S)
+        print(answer)
+
+# } Driver Code Ends
