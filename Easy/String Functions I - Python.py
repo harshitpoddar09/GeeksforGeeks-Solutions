aafb97096ce7c0e5a4fed76f96b6815fac95df82
Harshit Poddar <harshitpoddar09@gmail.com>
2021-12-16 11:44:46 +0530

    Add files via upload

---
+++ Easy/String Functions I - Python.py
@@ -0,0 +1,22 @@
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+
+def trim(str):
+    return str.strip()     
+
+def exists(str, x):
+    return str.find(x)     
+
+def titleIt(str):
+    return str.title()     
+
+def casesSwap(str):
+    return str.swapcase()     
+
+
+#{ 
+#Driver Code Starts.
