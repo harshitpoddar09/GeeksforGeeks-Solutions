d7abea906d7f4397337a52030298180b3dd720b5
Harshit Poddar <harshitpoddar09@gmail.com>
2021-12-16 11:44:32 +0530

    Add files via upload

---
+++ Easy/String Functions - II - Python.py
@@ -0,0 +1,33 @@
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+# Function to check if string 
+# starts and ends with 'gfg'
+def gfg(S):
+    b = S.lower()
+    if(b.startswith('gfg') and b.endswith('gfg')):  
+        print ("Yes")
+    else:
+        print ("No")
+
+#{ 
+#Driver Code Starts.
+# Driver Code
+def main():
+    # testcase input
+    testcases = int(input())
+    
+    # looping through testcases
+    while(testcases > 0):
+        S = input()
+        
+        testcases -= 1
+        
+        gfg(S)
+
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends
