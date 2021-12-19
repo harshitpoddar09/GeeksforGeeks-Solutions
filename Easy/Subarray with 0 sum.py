f68adfe7ca46fa401dd8a477ca30e04fa55f566a
Harshit Poddar <harshitpoddar09@gmail.com>
2021-12-19 20:11:05 +0530

    Add files via upload

---
+++ Easy/Subarray with 0 sum.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        ##Your code here
+        #Return true or false
+        if 0 in arr:
+            return True
+        for i in range(1,len(arr)):
+            arr[i]+=arr[i-1]
+        if 0 in arr:
+            return True
+        return len(set(arr))!=len(arr)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        if(Solution().subArrayExists(arr,n)):
+            print("Yes")
+        else:
+            print("No")
+        
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
