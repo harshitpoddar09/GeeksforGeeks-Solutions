94bfd9d7ed00a4118c561d898a84a574d955151d
Harshit Poddar <harshitpoddar09@gmail.com>
2021-12-22 16:01:38 +0530

    Add files via upload

---
+++ Easy/Reverse a string using Stack.py
@@ -0,0 +1,16 @@
+#Driver Code Starts
+
+ # } Driver Code Ends
+def reverse(S):
+    
+    #Add code here
+    return S[::-1]
+
+#{ 
+#Driver Code Starts.
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        str1=input()
+        print(reverse(str1))
+#} Driver Code Ends
