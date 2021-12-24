36d5f91ce7ce22cbfd42d04cd6495183e90c62b9
Harshit Poddar <harshitpoddar09@gmail.com>
2021-12-24 19:15:42 +0530

    Add files via upload

---
+++ Easy/Average_Min_Max - Python.py
@@ -0,0 +1,26 @@
+# Function to calculate average
+def calc_average(arr):
+    # Your code here
+    arr.remove(min(arr))
+    arr.remove(max(arr))
+    return sum(arr)//len(arr)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+# Driver Code
+if __name__ == '__main__':
+    # Testcase input
+    testcases = int(input())
+    # Looping through testcases
+    while(testcases > 0):
+        N = int(input())
+        
+        a = list(map(int,input().strip().split()))
+        
+        print (calc_average(a))
+        
+        testcases -= 1
+# } Driver Code Ends
